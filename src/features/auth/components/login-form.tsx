814aa2d49f148066bba5f7f7a88dc76777c208aa
karimm1620 <abdulkarimsulaeman2@gmail.com>
2025-11-09 09:10:25 +0700

    05: theme & styling

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -79,6 +79,7 @@ export function LoginForm() {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image src="/logos/github.svg" alt="GitHub" width={20} height={20} />
                     Continue with Github
                   </Button>
                   <Button
@@ -87,6 +88,7 @@ export function LoginForm() {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image src="/logos/google.svg" alt="Google" width={20} height={20} />
                     Continue with Google
                   </Button>
                 </div>
