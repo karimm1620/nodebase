37bcc14db090f98d3b3b0cc0ded1abaafda8ee69
karimm1620 <abdulkarimsulaeman2@gmail.com>
2025-12-02 19:26:42 +0700

    29: github google auth

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
-import { email, z } from "zod";
+import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -44,6 +44,32 @@ export function LoginForm() {
     },
   });
 
+  const signInGithub = async () => {
+    await authClient.signIn.social({
+      provider: "github",
+    }, {
+      onSuccess: () => {
+        router.push("/");
+      },
+      onError: () => {
+        toast.error("Something went wrong");
+      },
+    });
+  };
+
+  const signInGoogle = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+    }, {
+      onSuccess: () => {
+        router.push("/");
+      },
+      onError: () => {
+        toast.error("Something went wrong");
+      },
+    });
+  };
+
   const onSubmit = async (values: LoginFormValues) => {
     await authClient.signIn.email(
       {
@@ -77,6 +103,7 @@ export function LoginForm() {
               <div className="grid gap-6">
                 <div className="flex flex-col gap-4">
                   <Button
+                  onClick={signInGithub}
                     variant="outline"
                     className="w-full"
                     type="button"
@@ -90,7 +117,9 @@ export function LoginForm() {
                     />
                     Continue with Github
                   </Button>
+                  
                   <Button
+                    onClick={signInGoogle}
                     variant="outline"
                     className="w-full"
                     type="button"
